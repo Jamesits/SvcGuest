147ce49b40b0dd94a7b70cab38cc3d55f6946c81
James Swineson <github@public.swineson.me>
2018-09-16 20:48:20 +0800

    Bump version to 1.0.2.0

--- SvcGuest/Properties/AssemblyInfo.cs
+++ SvcGuest/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SvcGuest")]
+[assembly: AssemblyTitle("SvcGuest the Windows service wrapper")]
 [assembly: AssemblyDescription("Host any program as a Windows service.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("James Swineson")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.1")]
-[assembly: AssemblyFileVersion("1.0.1.1")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
