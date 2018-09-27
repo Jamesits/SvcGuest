7c2130f52059f87291c54022f2779fc2514f118e
James Swineson <github@public.swineson.me>
2018-09-27 14:52:33 +0800

    Add empty test project

---
+++ FakeTarget/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace FakeTarget
+{
+
+    [Command(Name = "FakeTarget.exe", Description = "Host any program as a Windows service.")]
+    class Program
+    {
+        #region Command Line Arguments
+        // ReSharper disable UnassignedGetOnlyAutoProperty
+        [Option("--file", Description = "Echo to which file")]
+        public string Filename { get; }
+
+        [Option("--onquit", Description = "Echo on quit")]
+        public string QuitTest { get; }
+
+        // ReSharper restore UnassignedGetOnlyAutoProperty
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+        #endregion
+
+
+        // ReSharper disable once UnusedMember.Global
+        public void OnExecute()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainUnhandledException;
+
+            // execute
+        }
+
+        /// <summary>
+        /// The default global exception handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs eventArgs)
+        {
+            var e = eventArgs.ExceptionObject as Exception;
+            Console.WriteLine(e?.InnerException);
+        }
+    }
+}
---
+++ SvcGuestTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SvcGuestTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SvcGuestTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c61f2cd-132d-4b37-97c4-93e3ed992bc9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
