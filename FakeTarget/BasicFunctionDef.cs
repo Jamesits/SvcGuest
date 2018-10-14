f2edde59f2eed22e8b5bef0eef16074ce58b49dd
James Swineson <github@public.swineson.me>
2018-10-14 15:39:06 +0800

    FakeTarget basic functions working

--- FakeTarget/BasicFunctionDef.cs
+++ FakeTarget/BasicFunctionDef.cs
@@ -1,13 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FakeTarget
 {
-    internal class BasicFunctionDef
+    internal static class BasicFunctionDef
     {
-        
+        internal static int Echo(string[] args)
+        {
+            Program.WriteLine(string.Join(" ", args.Skip(1).ToArray()));
+            return 0;
+        }
+
+        internal static int Delay(string[] args)
+        {
+            int delay = Convert.ToInt32(args[1]);
+            System.Threading.Thread.Sleep(delay);
+            return 0;
+        }
+
+        // ReSharper disable once IdentifierTypo
+        internal static int Lorem(string[] args)
+        {
+            // ReSharper disable StringLiteralTypo
+            // ReSharper disable once IdentifierTypo
+            // ReSharper disable once IdentifierTypo
+            const string loremIpsum =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            // ReSharper enable StringLiteralTypo
+            int chars = Convert.ToInt32(args[1]);
+            string output = loremIpsum;
+            while (output.Length < chars) output += loremIpsum;
+            Program.WriteLine(output.Take(chars).ToString());
+            return 0;
+        }
+
+        internal static int Quit(string[] args)
+        {
+            if (args.Length < 2) Environment.Exit(0);
+            else Environment.Exit(Convert.ToInt32(args[1]));
+
+            Program.CloseOutputStream();
+
+            // to make static checker happy
+            return 0;
+        }
+
+        internal static int GetCurrentUsername(string[] args)
+        {
+            Program.WriteLine(System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+            return 0;
+        }
     }
 }
--- FakeTarget/Program.cs
+++ FakeTarget/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 
 namespace FakeTarget
@@ -20,24 +21,72 @@ namespace FakeTarget
         public bool HideFromHelp { get; }
 
     }
-    class Program
+
+    internal static class Program
     {
 
         internal static readonly FuncDef[] Functions =
         {
             new FuncDef("help", Help, "Print this message"), 
+            new FuncDef("echo", BasicFunctionDef.Echo, "Echo the following text"),
+            new FuncDef("delay", BasicFunctionDef.Delay, "Sleep N milliseconds"),
+            // ReSharper disable once StringLiteralTypo
+            new FuncDef("lorem", BasicFunctionDef.Lorem, "Echo n chars of fake text"), 
+            new FuncDef("quit", BasicFunctionDef.Quit, "Quit program with optional ExitCode"),
+            new FuncDef("getUser", BasicFunctionDef.GetCurrentUsername, "Echo the username of current security context"), 
+            new FuncDef("setOutput", SetOutput, "Set the output mode to stdout|stderr|filename"), 
         };
 
         internal static int Help(string[] args)
         {
-            Console.WriteLine("A test program that does harmless pre-defined things for debug use.\n\nUsage: FakeTarget.exe [Options]\n\nOptions:");
+            Program.WriteLine("A test program that does harmless pre-defined things for debug use.\n\nUsage: FakeTarget.exe [Options]\n\nOptions:");
             foreach (var func in Functions.OrderBy(funcDef => funcDef.Command))
             {
                 if (!func.HideFromHelp)
-                    Console.WriteLine($"  -{func.Command}\t\t{func.Help}");
+                    Program.WriteLine($"  -{func.Command,-16} {func.Help}");
             }
-            Console.WriteLine();
+            Program.WriteLine();
             return 0;
+        }
+
+        internal static int SetOutput(string[] args)
+        {
+            if (string.Equals(args[1], "stdout", StringComparison.OrdinalIgnoreCase)) CurrentOutputMode = 0;
+            else if (string.Equals(args[1], "stderr", StringComparison.OrdinalIgnoreCase)) CurrentOutputMode = 1;
+            else
+            {
+                CurrentOutputStreamWriter = new StreamWriter(string.Join(" ", args.Skip(1).ToArray()));
+                CurrentOutputMode = 2;
+            }
+
+            if (CurrentOutputMode != 2) CloseOutputStream();
+
+            return 0;
+        }
+
+        // 0: stdout
+        // 1: stderr
+        // 2: file
+        internal static int CurrentOutputMode = 0;
+        internal static System.IO.StreamWriter CurrentOutputStreamWriter;
+
+        internal static void Write(string s = "")
+        {
+            if (CurrentOutputMode == 0) Console.Write(s);
+            else if (CurrentOutputMode == 1) Console.Error.Write(s);
+            else CurrentOutputStreamWriter.Write(s);
+        }
+
+        internal static void WriteLine(string s = "") => Write(s + "\n");
+
+        internal static void CloseOutputStream()
+        {
+            if (CurrentOutputStreamWriter != null)
+            {
+                CurrentOutputStreamWriter.Flush();
+                CurrentOutputStreamWriter.Close();
+                CurrentOutputStreamWriter = null;
+            }
         }
 
         /// <summary>
@@ -65,7 +114,7 @@ namespace FakeTarget
 
                 var currentCommand = currentArgs[0].TrimStart('-');
                 var currentFuncDef = Functions.Where(def =>
-                        string.Equals(def.Command.ToLower(), currentCommand.ToLower(), StringComparison.Ordinal))
+                        string.Equals(def.Command, currentCommand, StringComparison.OrdinalIgnoreCase))
                     .ToArray();
                 if (currentFuncDef.Length > 0)
                 {
@@ -79,6 +128,8 @@ namespace FakeTarget
                 }
             }
 
+            CloseOutputStream();
+
             return 0;
         }
 
@@ -90,7 +141,9 @@ namespace FakeTarget
         private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs eventArgs)
         {
             var e = eventArgs.ExceptionObject as Exception;
-            Console.WriteLine(e?.InnerException);
+            Program.WriteLine(e?.InnerException.ToString());
+
+            CloseOutputStream();
         }
     }
 }
