c758b123083c016964b49e8dd14969c0b58e6674
James Swineson <github@public.swineson.me>
2018-10-14 17:32:45 +0800

    Add some generic tests targeting the assembly

---
+++ SvcGuestTest/OverallTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SvcGuestTest
+{
+    [TestClass]
+    public class OverallTest
+    {
+        private string ProgramDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        [TestMethod]
+        public void IfRequiredFilesExist()
+        {
+            Assert.IsTrue(File.Exists(Path.Combine(ProgramDirectory, "SvcGuest.exe")));
+            Assert.IsTrue(File.Exists(Path.Combine(ProgramDirectory, "FakeTarget.exe")));
+        }
+    }
+}
